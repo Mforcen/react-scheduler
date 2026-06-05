f30b6709b697103409e73f9182c29560c2ae689d
forcen <me@mforcen.dev>
2026-06-05 17:47:38 +0200

    Migrated to viteplus

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,3 @@
-import { serve } from "bun";
-import index from "./index.html";
+import { Scheduler } from "./scheduler/Scheduler";
 
-const server = serve({
-  routes: {
-    // Serve index.html for all unmatched routes.
-    "/*": index,
-  },
-
-  development: process.env.NODE_ENV !== "production" && {
-    // Enable browser hot reloading in development
-    hmr: true,
-
-    // Echo console logs from the browser to the server
-    console: true,
-  },
-});
-
-console.log(`🚀 Server running at ${server.url}`);
+export default Scheduler;
